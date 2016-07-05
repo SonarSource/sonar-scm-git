0a679af2d01357bf8cda9d1bab37f4dafe0192a5
Julien HENRY <henryju@yahoo.fr>
2016-07-05 10:45:22 +0200

    Fix ITs

--- travis.sh
+++ travis.sh
@@ -10,12 +10,3 @@ configureTravis
 
 
 regular_mvn_build_deploy_analyze
-
-
-# Min supported version of SQ (5.0) can't be tested as it already embeds git 5.0.
-# Orchestrator does not allow to uninstall it.
-MIN_SQ_VERSION="5.1"
-echo '======= Run integration tests on minimal supported version of SonarQube ($MIN_SQ_VERSION)'
-./run_integration_tests.sh "$MIN_SQ_VERSION"
-
-# all other versions of SQ are tested by the QA pipeline at SonarSource
