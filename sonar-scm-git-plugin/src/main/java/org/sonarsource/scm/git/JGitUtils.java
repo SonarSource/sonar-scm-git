472e227a53a7f53858d6177a36031551698d9477
Duarte Meneses <duarte.meneses@sonarsource.com>
2019-01-23 09:08:50 +0100

    Fix code quality issue

--- sonar-scm-git-plugin/src/main/java/org/sonarsource/scm/git/JGitUtils.java
+++ sonar-scm-git-plugin/src/main/java/org/sonarsource/scm/git/JGitUtils.java
@@ -21,6 +21,7 @@ package org.sonarsource.scm.git;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import org.eclipse.jgit.lib.ObjectReader;
 import org.eclipse.jgit.lib.Repository;
 
 public class JGitUtils {
@@ -31,12 +32,13 @@ public class JGitUtils {
   public static Repository buildRepository(Path basedir) {
     try {
       Repository repo = GitScmProvider.getVerifiedRepositoryBuilder(basedir).build();
-      // SONARSCGIT-2 Force initialization of shallow commits to avoid later concurrent modification issue
-      repo.getObjectDatabase().newReader().getShallowCommits();
-      return repo;
+      try (ObjectReader objReader = repo.getObjectDatabase().newReader()) {
+        // SONARSCGIT-2 Force initialization of shallow commits to avoid later concurrent modification issue
+        objReader.getShallowCommits();
+        return repo;
+      }
     } catch (IOException e) {
       throw new IllegalStateException("Unable to open Git repository", e);
     }
   }
-
 }
